017736bda652db3eb968d093b83a2c94d1e9bfa1
syalciners <syalciners@gmail.com>
2026-08-15 02:15:26 +0300

    V1 ödev: WhatsApp stil katmanını yükle

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './lesson-form-fix.css'
 import './sheet-standard.css'
 import './sheet-footer-fixed.css'
 import './report-corporate.css'
+import './assignment-whatsapp.css'
 
 registerSW({ immediate: true })
 

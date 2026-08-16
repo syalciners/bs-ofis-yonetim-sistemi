0a1a53e848ef37f4f2dc91aa1dff73ecd91119a9
syalciners <syalciners@gmail.com>
2026-08-16 18:01:50 +0300

    Portal marka stilini etkinleştir

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './styles.css'
+import './brand.css'
 
 registerSW({ immediate: true })
 

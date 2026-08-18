d4d3e7a737f63a1b4a833bdc658a6064cbdf3250
syalciners <syalciners@gmail.com>
2026-08-19 01:50:19 +0300

    Kurum ayarları stillerini production girişine ekle

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,7 @@ import './premium-typography.css'
 import './page-title-standard.css'
 import './portal-preview.css'
 import './settings-hub.css'
+import './institution-settings.css'
 
 // Mevcut service worker varsa uygulama açılışında sunucudan güncel sürümü açıkça kontrol et.
 registerSW({

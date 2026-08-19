866223eeb36c559caff068ca2c4f6ad2b7ef86fe
syalciners <syalciners@gmail.com>
2026-08-19 15:45:21 +0300

    Koç Masası premium stillerini bağla

--- apps/bs-kocluk/src/main.tsx
+++ apps/bs-kocluk/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import './premium-dashboard.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

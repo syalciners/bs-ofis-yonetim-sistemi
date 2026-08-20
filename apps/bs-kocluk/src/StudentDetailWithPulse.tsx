770153d6e1cc5cf0deb6ffc2c416d9519925b19a
syalciners <syalciners@gmail.com>
2026-08-20 22:00:16 +0300

    Öğrenci 360'a çalışma ritmi ve gelişim panelini bağla

--- apps/bs-kocluk/src/StudentDetailWithPulse.tsx
+++ apps/bs-kocluk/src/StudentDetailWithPulse.tsx
@@ -3,6 +3,7 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import { studentName, type CoachData } from './data'
 import { ParentSummary, ParentSummaryButton } from './ParentSummary'
 import { StudentDetail } from './Student360'
+import { StudentDevelopmentPanel } from './StudentDevelopmentPanel'
 import { StudentPulsePanel } from './StudentPulsePanel'
 import { buildStudentPulse } from './studentPulse'
 
@@ -22,6 +23,7 @@ export function StudentDetailWithPulse({ data }: { data: CoachData }) {
 
   return <div className="student-pulse-route">
     <StudentPulsePanel pulse={pulse}/>
+    <StudentDevelopmentPanel data={data} studentId={studentId}/>
     <div className="student-detail-assistant-row"><ParentSummaryButton onClick={() => setParentSummaryOpen(true)}/></div>
     <StudentDetail data={data}/>
     {parentSummaryOpen && <ParentSummary studentId={studentId} studentName={name} onClose={() => setParentSummaryOpen(false)}/>} 

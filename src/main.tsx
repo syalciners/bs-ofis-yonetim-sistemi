d098904204a108b5fe620063f0b75cf6dbdfe6f6
syalciners <syalciners@gmail.com>
2026-08-21 22:02:33 +0300

    Grup dersleri sanat kartı stilini bağla

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import './portal-preview.css'
 import './brand-palette.css'
 import './demo-discovery.css'
 import './music-dance-art-direction.css'
+import './music-dance-groups-art.css'
 
 // Mevcut service worker varsa uygulama açılışında sunucudan güncel sürümü açıkça kontrol et.
 registerSW({

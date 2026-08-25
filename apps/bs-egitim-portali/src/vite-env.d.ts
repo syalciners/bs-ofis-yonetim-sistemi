834414d7123a09fab4b0954cbfaf6429dfd95d9a
syalciners <syalciners@gmail.com>
2026-08-25 03:34:40 +0300

    Portal instance env tiplerini ekle

--- apps/bs-egitim-portali/src/vite-env.d.ts
+++ apps/bs-egitim-portali/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL?: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string
+  readonly VITE_PORTAL_DESIGN_PREVIEW?: string
+  readonly VITE_BASE_PATH?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
